75d0c1d6c2283c1398a8c0c5ea66fbd52733a111
Vinicius-Costa-Oliveira <149127837+Vinicius-Costa-Oliveira@users.noreply.github.com>
2024-03-31 15:20:30 -0300

    Login e cadastro

---
+++ src/main/java/Conn.java
@@ -0,0 +1,24 @@
+package gerenciadorbiblioteca;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conn {
+     public static Connection connectToDatabase() {
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // O meu database chama teste, ajustem o de vcs nessa linha
+            con = DriverManager.getConnection("jdbc:mysql://localhost/teste?user=root&password="); 
+            //System.out.println("Conexão estabelecida com sucesso.");
+        } catch (ClassNotFoundException ex) {
+           // System.out.println("Driver do Banco não encontrado.");
+        } catch (SQLException ex) {
+            //System.out.println("Erro ao conectar ao banco de dados: " + ex.getMessage());
+        }
+        return con;
+    }
+}
+
+
