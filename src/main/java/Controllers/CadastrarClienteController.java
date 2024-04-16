da65426c0d0dc01a5344bbe8d2a7ccad9e76e31b
Pablo Duque <pabloduquesantos@gmail.com>
2024-04-16 13:39:45 -0300

    Cadastrar livros feito

--- src/main/java/Controllers/CadastrarClienteController.java
+++ src/main/java/Controllers/CadastrarClienteController.java
@@ -11,8 +11,6 @@ import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
--- src/main/java/Controllers/CadastrarLivroController.java
+++ src/main/java/Controllers/CadastrarLivroController.java
@@ -1,20 +1,28 @@
 package Controllers;
 
+import Banco.Database;
+import Modelos.Funcionario;
 import com.mycompany.gerenciadordebiblioteca.App;
 import java.io.IOException;
+import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 
 public class CadastrarLivroController {
 
     @FXML
-    private Button cadastrarFuncionario;
+    private Button cadastrarLivro;
 
     @FXML
-    private TextField inputAutor;
+    private ComboBox<String> inputAutor;
 
     @FXML
     private Label inputCPF;
@@ -26,7 +34,7 @@ public class CadastrarLivroController {
     private TextField inputFoto;
 
     @FXML
-    private TextField inputGenero;
+    private ComboBox<String> inputGenero;
 
     @FXML
     private Label inputNome;
@@ -41,18 +49,76 @@ public class CadastrarLivroController {
     private TextField inputTitulo;
 
     @FXML
-    private Label msgErro;
+    private Label messageLabel;
 
+     private Funcionario funcionario = Funcionario.getFuncionario("", "", "");
+    
+    public void initialize(URL url, ResourceBundle rb) {
+        try {
+            String query = "SELECT descricao FROM genero";
+            
+            ResultSet rs = Database.executarSelect(query);
+            while(rs.next())
+                inputGenero.getItems().add(rs.getString("descricao"));
+        } catch (SQLException | ClassNotFoundException ex) {
+            messageLabel.setTextFill(Color.color(1, 0, 0));
+            messageLabel.setText(ex.getMessage());
+        }
+        try {
+            String query = "SELECT nome FROM autor";
+            
+            ResultSet rs = Database.executarSelect(query);
+            while(rs.next())
+                inputAutor.getItems().add(rs.getString("nome"));
+        } catch (SQLException | ClassNotFoundException ex) {
+            messageLabel.setTextFill(Color.color(1, 0, 0));
+            messageLabel.setText(ex.getMessage());
+        }
+    }   
+     
+    @FXML
+    private void cadastrar(ActionEvent e){
+      // Executar a inserção no banco de dados
+        String titulo = inputTitulo.getText();
+        String autor = inputAutor.getValue();
+        String genero = inputGenero.getValue();
+        String foto = inputFoto.getText();
+        String qtd = inputQtdEstoque.getText();
+
+        if(titulo.isEmpty() || qtd.isEmpty() || foto.isEmpty() || autor == null || genero == null){
+            messageLabel.setTextFill(Color.color(1, 0, 0));
+            messageLabel.setText("Por favor, preencha todos os campos.");
+
+        }else try {
+                String query = "INSERT INTO livro (descricao, qtd_estoque, id_funcionario) VALUES ('" + titulo + "'," + qtd + ",'" + funcionario.getCpf() + "')";
+                Database.executarQuery(query);
+                query = ("INSERT INTO livros_autores(id_livro, id_autor) VALUES ((SELECT id FROM livro "
+                        + "WHERE descricao = '" + titulo +"'),(SELECT id FROM autor WHERE nome = '" + autor + "'))");
+                Database.executarQuery(query);
+                query = ("INSERT INTO livros_generos(id_livro, id_genero) VALUES ((SELECT id FROM livro "
+                        + "WHERE descricao = '" + titulo +"'),(SELECT id FROM genero WHERE descricao = '" + genero + "'))");
+                Database.executarQuery(query);
+                messageLabel.setTextFill(Color.color(0, 1, 0));
+                messageLabel.setText( "Retorne à página de login, funcionário cadastrado com sucesso.");
+            } catch (SQLException | ClassNotFoundException ex) {
+                messageLabel.setTextFill(Color.color(1, 0, 0));
+                messageLabel.setText(ex.getMessage());
+            }
+    }
+
+    
     @FXML
     void irPara(ActionEvent event) {
         try {
             App.mudarDeTela(event, "listarLivros");
         }
-        catch (IOException e) {
-            System.out.println("A tela \"listarLivros\" não foi encontrada.");
+        catch (IOException ex) {
+            messageLabel.setTextFill(Color.color(1, 0, 0));
+            messageLabel.setText(ex.getMessage());
         }
-        catch (Exception e) {
-            System.out.println(e.getMessage());
+        catch (Exception ex) {
+            messageLabel.setTextFill(Color.color(1, 0, 0));
+            messageLabel.setText(ex.getMessage());
         }
     }
 
