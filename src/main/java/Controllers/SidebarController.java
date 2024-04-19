1f002c7a45085e746261b627c2d4d1b336b258f6
bhPBB <bhpp.ezez@gmail.com>
2024-04-18 22:55:04 -0300

    - comecei o design da sidebar; - comecei o design do header das listas.

---
+++ src/main/java/Controllers/SidebarController.java
@@ -0,0 +1,43 @@
+package Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class SidebarController {
+
+    @FXML
+    private Label autores;
+
+    @FXML
+    private Label cliente;
+
+    @FXML
+    private Label emprestimosAtivos;
+
+    @FXML
+    private Label funcionarios;
+
+    @FXML
+    private Label generos;
+
+    @FXML
+    private Label livros;
+
+    @FXML
+    private Label painel;
+
+    @FXML
+    private Label usuario;
+
+    @FXML
+    void irPara(MouseEvent event) {
+
+    }
+
+    @FXML
+    void mudarCor(MouseEvent event) {
+
+    }
+
+}
