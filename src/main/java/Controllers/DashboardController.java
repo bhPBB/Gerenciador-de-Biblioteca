4d77bc3208e4a600f6c5c7790513539a30e9dceb
bhPBB <bhpp.ezez@gmail.com>
2024-04-21 10:28:50 -0300

    - fiz a dashboard; - mudei a interface de cadastro de livros.

---
+++ src/main/java/Controllers/DashboardController.java
@@ -0,0 +1,64 @@
+package Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class DashboardController {
+
+    @FXML
+    private Label qtdAutores;
+
+    @FXML
+    private Label qtdClientes;
+
+    @FXML
+    private Label qtdEmprestimosAtivos;
+
+    @FXML
+    private Label qtdEmprestimosMes;
+
+    @FXML
+    private Label qtdEmprestimosVencidos;
+
+    @FXML
+    private Label qtdGeneros;
+
+    @FXML
+    private Label qtdLivros;
+
+    @FXML
+    private Label sidebarAutores;
+
+    @FXML
+    private Label sidebarClientes;
+
+    @FXML
+    private Label sidebarEmprestimosAtivos;
+
+    @FXML
+    private Label sidebarFuncionarios;
+
+    @FXML
+    private Label sidebarGeneros;
+
+    @FXML
+    private Label sidebarLivros;
+
+    @FXML
+    private Label sidebarPainel;
+
+    @FXML
+    private Label siderbarUsuario;
+
+    @FXML
+    void irPara(MouseEvent event) {
+
+    }
+
+    @FXML
+    void mudarCor(MouseEvent event) {
+        
+    }
+
+}
