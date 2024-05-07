3bf32a0e580ec744db8a8e44c21beeec90ec1a0f
bhPBB <bhpp.ezez@gmail.com>
2024-05-06 23:16:27 -0300

    - Criei as views listarEmprestimosAtivos, listarAutores e listarGeneros

---
+++ src/main/java/Controllers/EmprestimosAtivosController.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package Controllers;
+
+import com.mycompany.gerenciadordebiblioteca.App;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author bruno
+ */
+public class EmprestimosAtivosController implements Initializable {
+
+    @FXML
+    private AnchorPane background;
+    
+    @FXML
+    private TableView<String> table; // mudar isso para 'Emprestimo'
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+          try
+        {
+            //Carrega a sidebar e o header
+            App.inicialzarSidebarHeader(
+                    "listarEmprestimosAtivos", 
+                    "Empréstimos Ativos", 
+                    "+", 
+                    "cadastrarEmprestimo", 
+                    background
+            );  
+        }
+        catch (IOException ex)
+        {
+            var msg = "Erro ao carregar a sideber e/ou header: " + ex.getMessage();
+            System.out.println(msg);
+        }
+    }    
+    
+}
